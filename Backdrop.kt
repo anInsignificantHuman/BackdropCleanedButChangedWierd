902cf0d305f3c9c373ec6406141f25517a0cd350
ethanlin271 <128556718+ethanlin271@users.noreply.github.com>
2023-11-12 13:56:21 -0500

    Update Backdrop.kt

--- Backdrop.kt
+++ Backdrop.kt
@@ -1,34 +1,22 @@
-class Backdrop2 {
+class Backdrop {
     private val canUseSetBonus = mutableListOf(true, true, true)
     var pixelsScored = mutableListOf<Pixel>()
 
-    var mosaicCount = 0
-    var setCount = 0
+    private var mosaicCount = 0
+    private var setCount = 0
 
-    private fun inPixelsScored(pixel: Hex) = pixelsScored.any { it.hex.q == pixel.q && it.hex.r == pixel.r && it.hex.s == pixel.s }
+    private fun inPixelsScored(pixel: Hex) = pixelsScored.any { it.hex == pixel }
 
-    fun scorePixel(pixel: Pixel): Backdrop2 {
+    fun scorePixel(pixel: Pixel): Backdrop {
         val pixelOffset = pixel.hex.cubeToOffset()
 
-        if (pixelsScored.any {
-                it.hex.q == pixel.hex.neighbor(Direction.BOTTOM_LEFT).q && it.hex.r == pixel.hex.neighbor(
-                    Direction.BOTTOM_LEFT
-                ).r && it.hex.s == pixel.hex.neighbor(Direction.BOTTOM_LEFT).s
-            } && pixelsScored.any {
-                it.hex.q == pixel.hex.neighbor(Direction.BOTTOM_RIGHT).q && it.hex.r == pixel.hex.neighbor(
-                    Direction.BOTTOM_RIGHT
-                ).r && it.hex.s == pixel.hex.neighbor(Direction.BOTTOM_RIGHT).s
-            }) {
+        pixelsScored.forEach { if(it == pixel) return this }
+
+        if (pixelsScored.any { it.hex == pixel.hex.neighbor(Direction.BOTTOM_LEFT) } && pixelsScored.any { it.hex == pixel.hex.neighbor(Direction.BOTTOM_RIGHT) }) {
             pixelsScored.add(pixel)
         } else if (pixel.hex.r == 0) {
             pixelsScored.add(pixel)
-        } else if (pixelOffset.x == 0 && pixelOffset.y % 2 == 1 && pixelsScored.any {
-                it.hex.q == pixel.hex.neighbor(
-                    Direction.BOTTOM_RIGHT
-                ).q && it.hex.r == pixel.hex.neighbor(Direction.BOTTOM_RIGHT).r && it.hex.s == pixel.hex.neighbor(
-                    Direction.BOTTOM_RIGHT
-                ).s
-            }) {
+        } else if (pixelOffset.x == 0 && pixelOffset.y % 2 == 1 && pixelsScored.any { it.hex == pixel.hex.neighbor(Direction.BOTTOM_RIGHT) }) {
             pixelsScored.add(pixel)
         } else if (pixelOffset.x == 6 && pixelOffset.y % 2 == 1) {
             pixelsScored.add(pixel)
@@ -38,7 +26,7 @@ class Backdrop2 {
         return this
     }
 
-    private fun countScorePixels(): Int {
+    private fun countPixelScore(): Int {
         var score = 0
         pixelsScored.forEach { _ -> score += Points.PIXEL_POINTS.amount }
         return score
@@ -96,7 +84,7 @@ class Backdrop2 {
         var neighbors: MutableList<Pixel>
         val pixelsToRemove = mutableListOf<Pixel>()
 
-        score += countScorePixels()
+        score += countPixelScore()
 
         for (pixel in pixelsScored) {
             if (reachesSetLine(pixel)) {
@@ -122,25 +110,26 @@ class Backdrop2 {
     }
 
     fun sortBoard() {
-//        pixelsScored.sortBy { it.hex.q }
-//        pixelsScored.sortByDescending { it.hex.r }
+        pixelsScored.sortBy { it.hex.q }
+        pixelsScored.sortByDescending { it.hex.r }
     }
 
-    private fun populateAvailableMoves(pixel: Hex, direction: Direction): Collection<Pixel> {
-        return Color.values().map { Pixel(pixel.neighbor(direction), it) }
-    }
+    private fun populateAvailableMoves(pixel: Hex, direction: Direction) = Color.values().map { Pixel(pixel.neighbor(direction), it) }
 
-    private fun pixelPlaceable(pixel: Pixel, direction1: Direction, direction2: Direction): Boolean {
-        return pixel.hasNeighborInList(
-            pixelsScored,
-            direction1
-        ) && !(this.inPixelsScored(pixel.hex.neighbor((direction2))))
-    }
+    private fun populateAvailableMoves(pixel: Hex) = Color.values().map { Pixel(pixel, it) }
+
+    private fun pixelPlaceable(pixel: Pixel, direction1: Direction, direction2: Direction) = pixel.hasNeighborInList(pixelsScored, direction1) && !this.inPixelsScored(pixel.hex.neighbor(direction2))
 
     private fun pixelPlaceableOddCorners(pixel: Pixel): Boolean {
-        return (pixel.hex.cubeToOffset().x == 0 && !(this.inPixelsScored(pixel.hex.neighbor((Direction.TOP_LEFT))))) || (pixel.hex.cubeToOffset().x == 5 && !(this.inPixelsScored(
-            pixel.hex.neighbor((Direction.TOP_RIGHT))
-        )))
+        val x = pixel.hex.cubeToOffset().x
+        val y = pixel.hex.cubeToOffset().y
+        val hex = pixel.hex
+        val tlInPixelsScored = this.inPixelsScored(hex.neighbor(Direction.TOP_LEFT))
+        val trInPixelsScored = this.inPixelsScored(hex.neighbor(Direction.TOP_RIGHT))
+        val isTopLeft = x == 0
+        val isTopRight = y == 5
+
+        return (isTopLeft && !tlInPixelsScored) || (isTopRight && !trInPixelsScored)
     }
 
     fun getAvailableMoves(): HashSet<Pixel> {
@@ -151,11 +140,7 @@ class Backdrop2 {
             evenFlags[pixel.hex.cubeToOffset().x] = true
         }
 
-        evenFlags.forEachIndexed { index, flag ->
-            if (!flag) {
-                Color.values().forEach { availableMoves.add(Pixel((Offset(index, 0)).offsetToCube(), it)) }
-            }
-        }
+        evenFlags.forEachIndexed { index, flag -> if (!flag) availableMoves.addAll(populateAvailableMoves((Offset(index, 0)).offsetToCube())) }
 
         for (pixel in pixelsScored.filter { it.hex.r % 2 != 0 && it.hex.cubeToOffset().y <= 9 }) {
             if (pixelPlaceable(pixel, Direction.LEFT, Direction.TOP_LEFT)) {
@@ -169,7 +154,6 @@ class Backdrop2 {
         for (pixel in pixelsScored.filter { it.hex.r % 2 == 0 && it.hex.cubeToOffset().y < 10 }) {
             if (pixelPlaceable(pixel, Direction.RIGHT, Direction.TOP_RIGHT)) {
                 availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_RIGHT))
-
             }
             if (pixelPlaceable(pixel, Direction.RIGHT, Direction.TOP_RIGHT)) {
                 availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_RIGHT))
@@ -185,6 +169,67 @@ class Backdrop2 {
             availableMoves.distinctBy { "${it.hex.q}-${it.hex.r}-${it.hex.s}-${it.color}" }.toHashSet()
 
         return availableMoves
+    }
+
+    fun printBoard() {
+        val numRows = 11
+        var board = mutableListOf<MutableList<String>>()
+
+        val colorMap = mapOf(
+            Color.WHITE to "W",
+            Color.GREEN to "G",
+            Color.YELLOW to "Y",
+            Color.PURPLE to "P"
+        )
+
+        for (rowIndex in 0 until numRows) {
+            val numCols = if (rowIndex % 2 == 0) 6 else 7
+            val row = MutableList(numCols) { "" }
+            board.add(row)
+        }
+
+
+//        val evenRow = mutableListOf(" ", "X", "X", "X", "X", "X", "X", " ")
+//        val oddRow = mutableListOf("X", "X", "X", "X", "X", "X", "X", "X")
+
+        for (i in 0..10) {
+            val evenRow = mutableListOf(" ", " ", " ", " ", " ", " ", " ", " ")
+            val oddRow = mutableListOf(" ", " ", " ", " ", " ", " ", " ")
+
+            if (i % 2 == 0) {
+                board[i] = evenRow  // Create a new list instance for even rows
+            } else {
+                board[i] = oddRow   // Create a new list instance for odd rows
+            }
+        }
+
+
+        pixelsScored.forEach { pixel ->
+            val offset = pixel.hex.cubeToOffset()
+            val row = offset.y
+            val col = offset.x
+            val color = pixel.color
+
+            if(row % 2 == 0) {
+                board[row][col + 1] = colorMap[color].toString()
+            }
+            else {
+                board[row][col] = colorMap[color].toString()
+            }
+
+        }
+
+        board = board.reversed().toMutableList()
+
+        board.forEachIndexed { index, it ->
+            if(index % 2 == 0) {
+                println(it.joinToString("  "))
+            }
+            else {
+                println(" ${it.joinToString("  ")}")
+            }
+        }
+
     }
 }
 
@@ -208,5 +253,20 @@ fun main() {
 //
 //    println(backdrop.calculateScore())
 //    println(backdrop.getAvailableMoves())
+//
+//    val backdrop = Backdrop()
+//    val pixel1 = Pixel(Hex(0, 0, 0), Color.WHITE)
+//    val pixel2 = Pixel(Hex(1, 0, -1), Color.WHITE)
+//    val pixel3 = Pixel(Hex(0, -1, 1), Color.GREEN)
+//    val pixel4 = Pixel(Hex(1, -1, 0), Color.GREEN)
+//    val pixel5 = Pixel(Hex(1, -2, 1), Color.GREEN)
+//
+//    backdrop.scorePixel(pixel1)
+//    backdrop.scorePixel(pixel2)
+//    backdrop.scorePixel(pixel3)
+//    backdrop.scorePixel(pixel4)
+//    backdrop.scorePixel(pixel5)
+//
+//    backdrop.printBoard()
 
 }
