7bd1107979991d321e22b1c061cc3fc99b1d1793
anInsignificantHuman <68439743+anInsignificantHuman@users.noreply.github.com>
2023-11-18 15:57:53 -0500

    Update Minimax.kt

--- Minimax.kt
+++ Minimax.kt
@@ -1,8 +1,8 @@
 var bestValue = Int.MIN_VALUE
 var bestMovePath = mutableListOf<Pixel>()
 
-fun weightMove(currBackdrop: Backdrop2, move: Pixel): Int {
-    val tempBackdrop = Backdrop2()
+fun weightMove(currBackdrop: Backdrop, move: Pixel): Int {
+    val tempBackdrop = Backdrop()
     var weight = 0
     currBackdrop.pixelsScored.forEach { tempBackdrop.scorePixel(it) }
     tempBackdrop.scorePixel(move)
@@ -18,7 +18,12 @@ fun weightMove(currBackdrop: Backdrop2, move: Pixel): Int {
     return weight
 }
 
-fun minimax(backdrop: Backdrop2, depth: Int, movePath: List<Pixel> = listOf()): Int {
+fun minimax(backdrop: Backdrop, depth: Int, movePath: List<Pixel> = listOf(), currentEval: Int = 0): Int {
+//    if (depth == 0 || depth == 1) {
+//        val move = movePath.last()
+//        if (move.color != Color.WHITE && !backdrop.isMosaicPossibleNextTwoMoves(move, backdrop)) return Int.MIN_VALUE
+//    }
+
     if (depth == 0 || backdrop.getAvailableMoves().isEmpty()) {
         return backdrop.calculateScore()
     }
@@ -27,16 +32,21 @@ fun minimax(backdrop: Backdrop2, depth: Int, movePath: List<Pixel> = listOf()): 
         return Int.MIN_VALUE
     }
 
+    if (backdrop.calculateScore() < currentEval) {
+        return Int.MIN_VALUE
+    }
+
 //    backdrop.getAvailableMoves().sortedByDescending {
 //        weightMove(backdrop, it)
 //    }
 
     for (move in backdrop.getAvailableMoves()) {
-        val newBackdrop = Backdrop2()
+        val newBackdrop = Backdrop()
         backdrop.pixelsScored.forEach { newBackdrop.scorePixel(it) }
+
         newBackdrop.scorePixel(move)
 
-        val newValue = minimax(newBackdrop, depth - 1, movePath + move)
+        val newValue = minimax(newBackdrop, depth - 1, movePath + move, backdrop.calculateScore())
 
         if (bestValue < newValue) {
             bestValue = newValue
@@ -47,31 +57,31 @@ fun minimax(backdrop: Backdrop2, depth: Int, movePath: List<Pixel> = listOf()): 
     return bestValue
 }
 
-
 fun main() {
-    val backdrop = Backdrop2()
-    backdrop.scorePixel(Pixel(Hex(1, 0, -1), Color.WHITE))
-    backdrop.scorePixel(Pixel(Hex(3, 0, -3), Color.YELLOW))
-    backdrop.scorePixel(Pixel(Hex(4, 0, -4), Color.YELLOW))
-    backdrop.scorePixel(Pixel(Hex(4, -1, -3), Color.WHITE))
+    val backdrop = Backdrop()
+//    backdrop.scorePixel(Pixel(Hex(1, 0, -1), Color.WHITE))
+//    backdrop.scorePixel(Pixel(Hex(3, 0, -3), Color.YELLOW))
+//    backdrop.scorePixel(Pixel(Hex(4, 0, -4), Color.YELLOW))
+//    backdrop.scorePixel(Pixel(Hex(4, -1, -3), Color.WHITE))
+    println(backdrop.pixelsLeft)
 
     val timeStart = System.currentTimeMillis()
 
-    println(minimax(backdrop, 5))
-    println(bestMovePath)
-    backdrop.pixelsScored.addAll(bestMovePath)
+    for (i in 0..1) {
+        println(minimax(backdrop, 5))
+        println(bestMovePath)
+        backdrop.scorePixel(*bestMovePath.toTypedArray())
+        println(backdrop.pixelsLeft)
+    }
 
-    println(minimax(backdrop, 5))
-    println(bestMovePath)
-    backdrop.pixelsScored.addAll(bestMovePath)
+    for (i in 0..4){
+        println(minimax(backdrop, 6))
+        println(bestMovePath)
+        backdrop.scorePixel(*bestMovePath.toTypedArray())
+        println(backdrop.pixelsLeft)
+    }
 
-    println(minimax(backdrop, 5))
-    println(bestMovePath)
-    backdrop.pixelsScored.addAll(bestMovePath)
-
-    println(minimax(backdrop, 5))
-    println(bestMovePath)
-    backdrop.pixelsScored.addAll(bestMovePath)
+    backdrop.printBoard()
 
     println(System.currentTimeMillis() - timeStart)
 //    backdrop.scorePixel(Pixel(Hex(0, 0, 0), Color.YELLOW))
