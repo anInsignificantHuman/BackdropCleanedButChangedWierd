3332d3a14c6090ffb1596d79e1f7c10d693a11fd
anInsignificantHuman <68439743+anInsignificantHuman@users.noreply.github.com>
2023-11-18 15:57:09 -0500

    Update Backdrop.kt

--- Backdrop.kt
+++ Backdrop.kt
@@ -5,7 +5,18 @@ class Backdrop {
     private var mosaicCount = 0
     private var setCount = 0
 
+    var pixelsLeft = mutableMapOf(
+        Color.WHITE  to 25,
+        Color.GREEN  to 5,
+        Color.YELLOW to 5,
+        Color.PURPLE to 5,
+    )
+
     private fun inPixelsScored(pixel: Hex) = pixelsScored.any { it.hex == pixel }
+
+    fun scorePixel(vararg pixels: Pixel) {
+        pixels.forEach { scorePixel(it) }
+    }
 
     fun scorePixel(pixel: Pixel): Backdrop {
         val pixelOffset = pixel.hex.cubeToOffset()
@@ -14,12 +25,16 @@ class Backdrop {
 
         if (pixelsScored.any { it.hex == pixel.hex.neighbor(Direction.BOTTOM_LEFT) } && pixelsScored.any { it.hex == pixel.hex.neighbor(Direction.BOTTOM_RIGHT) }) {
             pixelsScored.add(pixel)
+            pixelsLeft[pixel.color] = pixelsLeft[pixel.color]!! - 1
         } else if (pixel.hex.r == 0) {
             pixelsScored.add(pixel)
+            pixelsLeft[pixel.color] = pixelsLeft[pixel.color]!! - 1
         } else if (pixelOffset.x == 0 && pixelOffset.y % 2 == 1 && pixelsScored.any { it.hex == pixel.hex.neighbor(Direction.BOTTOM_RIGHT) }) {
             pixelsScored.add(pixel)
+            pixelsLeft[pixel.color] = pixelsLeft[pixel.color]!! - 1
         } else if (pixelOffset.x == 6 && pixelOffset.y % 2 == 1) {
             pixelsScored.add(pixel)
+            pixelsLeft[pixel.color] = pixelsLeft[pixel.color]!! - 1
         } else {
             println("Unable to add your pixel because it does not obey pixel addition constraints")
         }
@@ -114,9 +129,13 @@ class Backdrop {
         pixelsScored.sortByDescending { it.hex.r }
     }
 
-    private fun populateAvailableMoves(pixel: Hex, direction: Direction) = Color.values().map { Pixel(pixel.neighbor(direction), it) }
+    private fun populateAvailableMoves(pixel: Hex, direction: Direction) = Color.values()
+        .filter { pixelsLeft[it]!! > 0 }
+        .map { Pixel(pixel.neighbor(direction), it) }
 
-    private fun populateAvailableMoves(pixel: Hex) = Color.values().map { Pixel(pixel, it) }
+    private fun populateAvailableMoves(pixel: Hex) = Color.values()
+        .filter { pixelsLeft[it]!! > 0 }
+        .map { Pixel(pixel, it) }
 
     private fun pixelPlaceable(pixel: Pixel, direction1: Direction, direction2: Direction) = pixel.hasNeighborInList(pixelsScored, direction1) && !this.inPixelsScored(pixel.hex.neighbor(direction2))
 
@@ -127,7 +146,7 @@ class Backdrop {
         val tlInPixelsScored = this.inPixelsScored(hex.neighbor(Direction.TOP_LEFT))
         val trInPixelsScored = this.inPixelsScored(hex.neighbor(Direction.TOP_RIGHT))
         val isTopLeft = x == 0
-        val isTopRight = y == 5
+        val isTopRight = x == 5
 
         return (isTopLeft && !tlInPixelsScored) || (isTopRight && !trInPixelsScored)
     }
@@ -140,7 +159,11 @@ class Backdrop {
             evenFlags[pixel.hex.cubeToOffset().x] = true
         }
 
-        evenFlags.forEachIndexed { index, flag -> if (!flag) availableMoves.addAll(populateAvailableMoves((Offset(index, 0)).offsetToCube())) }
+        evenFlags.forEachIndexed { index, flag ->
+            if (!flag) {
+                availableMoves.addAll(populateAvailableMoves((Offset(index, 0)).offsetToCube()))
+            }
+        }
 
         for (pixel in pixelsScored.filter { it.hex.r % 2 != 0 && it.hex.cubeToOffset().y <= 9 }) {
             if (pixelPlaceable(pixel, Direction.LEFT, Direction.TOP_LEFT)) {
@@ -155,11 +178,16 @@ class Backdrop {
             if (pixelPlaceable(pixel, Direction.RIGHT, Direction.TOP_RIGHT)) {
                 availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_RIGHT))
             }
-            if (pixelPlaceable(pixel, Direction.RIGHT, Direction.TOP_RIGHT)) {
-                availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_RIGHT))
+            if (pixelPlaceable(pixel, Direction.LEFT, Direction.TOP_LEFT)) {
+                availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_LEFT))
             }
             if (pixelPlaceableOddCorners(pixel)) {
-                availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_LEFT))
+                if (pixel.hex.cubeToOffset().x == 0) {
+                    availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_LEFT))
+                }
+                else {
+                    availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_RIGHT))
+                }
             } else if (pixelPlaceableOddCorners(pixel)) {
                 availableMoves.addAll(populateAvailableMoves(pixel.hex, Direction.TOP_RIGHT))
             }
@@ -171,16 +199,80 @@ class Backdrop {
         return availableMoves
     }
 
+    fun canScore(pixel: Hex): Boolean {
+        val pixelOffset = pixel.cubeToOffset()
+
+        return if (pixelsScored.any { it.hex == pixel.neighbor(Direction.BOTTOM_LEFT) } && pixelsScored.any { it.hex == pixel.neighbor(Direction.BOTTOM_RIGHT) }) true
+        else if (pixel.r == 0) true
+        else if (pixelOffset.x == 0 && pixelOffset.y % 2 == 1 && pixelsScored.any { it.hex == pixel.neighbor(Direction.BOTTOM_RIGHT) }) true
+        else if (pixelOffset.x == 6 && pixelOffset.y % 2 == 1) true
+        else false
+
+    }
+
+    fun isMosaicPossibleNextTwoMoves(pixel: Pixel, backdrop: Backdrop): Boolean {
+//        if(pixel.hex.cubeToOffset().x != 0 || !(pixel.hex.cubeToOffset().x != 6 && pixel.hex.cubeToOffset().y % 2 != 0) || !(pixel.hex.cubeToOffset().x != 5 && pixel.hex.cubeToOffset().y % 2 == 0)) {
+//            if (canScore(pixel.hex.neighbor(Direction.LEFT))) return true
+//            if (canScore(pixel.hex.neighbor(Direction.RIGHT))) return true
+//            if (canScore(pixel.hex.neighbor(Direction.TOP_LEFT)) && canScore(pixel.hex.neighbor(Direction.TOP_RIGHT))) return true
+//        }
+//        else return false
+//        val x = pixel.hex.cubeToOffset().x
+//        val y = pixel.hex.cubeToOffset().y
+//
+//        if(y == 10) return false
+//
+//        if(x == 0) {
+//            if(canScore(pixel.hex.neighbor(Direction.RIGHT))) return true
+//        }
+//        else if((x == 5 && y % 2 == 0) || x == 6) {
+//            if(canScore(pixel.hex.neighbor(Direction.LEFT))) return true
+//        }
+//        else {
+//            if (canScore(pixel.hex.neighbor(Direction.LEFT))) return true
+//            if (canScore(pixel.hex.neighbor(Direction.RIGHT))) return true
+//            if (canScore(pixel.hex.neighbor(Direction.TOP_LEFT)) && canScore(pixel.hex.neighbor(Direction.TOP_RIGHT))) return true
+//        }
+//
+//        return false
+
+        backdrop.scorePixel(pixel)
+        var availableMoves = backdrop.getAvailableMoves()
+        availableMoves = availableMoves.filter { pixel.hex.allNeighbors().contains(it.hex) }.toHashSet()
+        availableMoves.forEach { move ->
+            val tempBackdrop = Backdrop()
+            tempBackdrop.scorePixel(*backdrop.pixelsScored.toTypedArray())
+            tempBackdrop.scorePixel(move)
+
+            tempBackdrop.getAvailableMoves().filter { pixel.hex.allNeighbors().contains(it.hex) }.forEach { currPixel ->
+                tempBackdrop.scorePixel(currPixel)
+                if(isPartOfMosaic(currPixel, tempBackdrop.filterNonColored())) {
+                    return true
+                }
+            }
+        }
+
+        return false
+
+//        if(isPartOfMosaic(pixel, backdrop.filterNonColored())) return true
+//        else {
+//            availableMoves.forEach {
+//                val temp = backdrop
+//                temp.scorePixel(it)
+//                val colored = temp.filterNonColored()
+//                if (isPartOfMosaic(it, colored)) {
+//                    return true
+//                }
+//            }
+//        }
+//
+//        return false
+
+    }
+
     fun printBoard() {
         val numRows = 11
         var board = mutableListOf<MutableList<String>>()
-
-        val colorMap = mapOf(
-            Color.WHITE to "W",
-            Color.GREEN to "G",
-            Color.YELLOW to "Y",
-            Color.PURPLE to "P"
-        )
 
         for (rowIndex in 0 until numRows) {
             val numCols = if (rowIndex % 2 == 0) 6 else 7
@@ -211,10 +303,10 @@ class Backdrop {
             val color = pixel.color
 
             if(row % 2 == 0) {
-                board[row][col + 1] = colorMap[color].toString()
+                board[row][col + 1] = color.tag
             }
             else {
-                board[row][col] = colorMap[color].toString()
+                board[row][col] = color.tag
             }
 
         }
@@ -234,39 +326,12 @@ class Backdrop {
 }
 
 fun main() {
-//    println()
-//
-//    val backdrop = Backdrop()
-//    val pixel1 = Pixel(Hex(0, 0, 0), Color.WHITE)
-//    val pixel2 = Pixel(Hex(1, 0, -1), Color.WHITE)
-//    val pixel3 = Pixel(Hex(0, -1, 1), Color.GREEN)
-//    val pixel4 = Pixel(Hex(1, -1, 0), Color.GREEN)
-//    val pixel5 = Pixel(Hex(1, -2, 1), Color.GREEN)
-//
-//    backdrop.scorePixel(pixel1)
-//    backdrop.scorePixel(pixel2)
-//    backdrop.scorePixel(pixel3)
-//    backdrop.scorePixel(pixel4)
-//    backdrop.scorePixel(pixel5)
-//
-//    backdrop.sortBoard()
-//
-//    println(backdrop.calculateScore())
-//    println(backdrop.getAvailableMoves())
-//
-//    val backdrop = Backdrop()
-//    val pixel1 = Pixel(Hex(0, 0, 0), Color.WHITE)
-//    val pixel2 = Pixel(Hex(1, 0, -1), Color.WHITE)
-//    val pixel3 = Pixel(Hex(0, -1, 1), Color.GREEN)
-//    val pixel4 = Pixel(Hex(1, -1, 0), Color.GREEN)
-//    val pixel5 = Pixel(Hex(1, -2, 1), Color.GREEN)
-//
-//    backdrop.scorePixel(pixel1)
-//    backdrop.scorePixel(pixel2)
-//    backdrop.scorePixel(pixel3)
-//    backdrop.scorePixel(pixel4)
-//    backdrop.scorePixel(pixel5)
-//
-//    backdrop.printBoard()
+    val pixel1 = Pixel(Hex(0, 0, 0), Color.GREEN)
+    val pixel2 = Pixel(Hex(1, 0, -1), Color.GREEN)
+    val pixel3 = Pixel(Hex(1, -1, 0), Color.GREEN)
+
+    val backdrop = Backdrop()
+
+    println(backdrop.isMosaicPossibleNextTwoMoves(pixel1, backdrop))
 
 }
